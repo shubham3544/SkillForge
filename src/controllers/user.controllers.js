f81d13aa8e3bf07dccde1b0d14065b451ea43ff2
Shubham Nagpal <shubhamnagpal018@gmail.com>
2026-07-18 01:30:50 +0530

    feat(user): implement user profile management APIs

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -278,6 +278,109 @@ const changeCurrentPassword = asyncHandler(async (req,res) => {
 
 });
 
+const updateAccountDetails = asyncHandler(async(req,res)=> {
+
+   const {fullname, email} = req.body|| {};
+
+   if (!fullname || !email) {
+    throw new ApiError(400, "Full name and email are required");
+}
+
+   const user = await User.findByIdAndUpdate(
+      req.user?._id,
+      {
+         $set: {
+            fullname,
+            email
+         }
+      },
+      {
+         new: true,
+      }
+   ).select("-password -refreshToken");
+
+   return res.status(200).json(
+    new ApiResponse(
+        200,
+        user,
+        "Account details updated successfully"
+      )
+   );
+   
+});
+
+const updateUserAvatar = asyncHandler(async(req,res) => {
+  
+   const avatarLocalPath = req.file?.path;
+
+   if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+   }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+    if (!avatar) {
+    throw new ApiError(500, "Error while uploading avatar");
+    } 
+
+    const user = await User.findByIdAndUpdate(
+      req.user?._id,
+      {
+         $set:{
+            avatar: avatar.url
+         }
+      },
+      {
+         new: true
+      }
+     ).select("-password -refreshToken")
+
+     return res.status(200).json(
+    new ApiResponse(
+        200,
+        user,
+        "Avatar updated successfully"
+    )
+);
+    
+    
+});
+
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+
+    const coverImageLocalPath = req.file?.path;
+
+    if (!coverImageLocalPath) {
+        throw new ApiError(400, "Cover image file is required");
+    }
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if (!coverImage) {
+        throw new ApiError(500, "Error while uploading cover image");
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                coverImage: coverImage.url
+            }
+        },
+        {
+            new: true
+        }
+    ).select("-password -refreshToken");
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            user,
+            "Cover image updated successfully"
+        )
+    );
+});
+
     
 
     
@@ -285,5 +388,8 @@ const changeCurrentPassword = asyncHandler(async (req,res) => {
          loginUser,
          logoutUser,
          refreshAccessToken,
-         changeCurrentPassword
+         changeCurrentPassword,
+         updateAccountDetails,
+         updateUserAvatar,
+         updateUserCoverImage
       };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import  {registerUser, loginUser, logoutUser, refreshAccessToken , changeCurrentPassword} from "../controllers/user.controllers.js"
+import  {registerUser, loginUser, logoutUser, refreshAccessToken , changeCurrentPassword, updateAccountDetails, updateUserAvatar,updateUserCoverImage} from "../controllers/user.controllers.js"
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -22,7 +22,18 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
-router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+router.route("/avatar").patch(
+    verifyJWT,
+    upload.single("avatar"),
+    updateUserAvatar
+);
+router.route("/cover-image").patch(
+    verifyJWT,
+    upload.single("coverImage"),
+    updateUserCoverImage
+);
 
 
 export default router;
