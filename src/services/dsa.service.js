0c299b0c2a6b39becf38fbb74dc55fb4c8212f7b
Shubham Nagpal <shubhamnagpal018@gmail.com>
2026-08-09 17:52:35 +0530

    feat(activity): complete DSA activity integration

--- src/services/dsa.service.js
+++ src/services/dsa.service.js
@@ -1,6 +1,7 @@
 import { DSAProblem } from "../models/dsaProblem.models.js";
 import { Pattern } from "../models/patterns.models.js";
 import { ApiError } from "../utils/ApiError.js";
+import { createActivity } from "./activity.services.js";
 
 const createProblemService = async (userId, problemData) => {
 
@@ -53,6 +54,18 @@ const createProblemService = async (userId, problemData) => {
         pattern,
         notes,
         solvedAt,
+    });
+
+    await createActivity({
+        user: userId,
+        type: "DSA_CREATED",
+        message: `Added DSA problem ${problem.title}`,
+        metadata: {
+            problemid: problem._id,
+            problemTitle: problem.title,
+            platform:  problem.platform,
+            difficulty: problem.difficulty,
+        },
     });
 
     return problem;
@@ -108,7 +121,10 @@ const updateProblemService = async (userId, problemId, problemData) => {
         throw new ApiError(404, "Problem not found");
     }
 
+    const oldStatus = problem.status;
+
     if (pattern) {
+
         const patternExists = await Pattern.findOne({
             _id: pattern,
             user: userId,
@@ -153,6 +169,45 @@ const updateProblemService = async (userId, problemId, problemData) => {
 
     await problem.save();
 
+    // Status based activity
+    if (oldStatus !== "Solved" && problem.status === "Solved") {
+
+        await createActivity({
+            user: userId,
+            type: "DSA_SOLVED",
+            message: `Solved DSA problem ${problem.title}`,
+            metadata: {
+                problemId: problem._id,
+                problemTitle: problem.title,
+                difficulty: problem.difficulty,
+            },
+        });
+
+    } else if (oldStatus !== "Revisit" && problem.status === "Revisit") {
+
+        await createActivity({
+            user: userId,
+            type: "DSA_REVISIT",
+            message: `Marked DSA problem ${problem.title} for revisit`,
+            metadata: {
+                problemId: problem._id,
+                problemTitle: problem.title,
+            },
+        });
+
+    } else {
+
+        await createActivity({
+            user: userId,
+            type: "DSA_UPDATED",
+            message: `Updated DSA problem ${problem.title}`,
+            metadata: {
+                problemId: problem._id,
+                problemTitle: problem.title,
+            },
+        });
+    }
+
     return problem;
 };
 
@@ -172,9 +227,18 @@ const deleteProblemService = async (userId, problemId) => {
         user: userId,
     });
 
+    await createActivity({
+        user: userId,
+        type: "DSA_DELETED",
+        message: `Deleted DSA problem ${problem.title}`,
+        metadata: {
+            problemId: problem._id,
+            problemTitle: problem.title,
+        },
+    });
+
     return problem;
 };
-
 
 
 
